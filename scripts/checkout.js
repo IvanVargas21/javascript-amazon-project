7e5e604452ee0e1e0c9cc0637ca3b28087096bde
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2024-11-08 14:50:01 +0800

    Added: Convert products into classes.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkOutHeader.js";
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 import isSatSun from "./checkSatSun.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 renderCheckoutHeader();
